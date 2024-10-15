124b07f508161c5ddb81bbc17371209238a0e813
Rastko K <63473032+notrastko@users.noreply.github.com>
2024-10-15 14:01:34 -0700

    Initial Commit

---
+++ Sendhooks/SendhooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendhooksApp.swift
+//  Sendhooks
+//
+//  Created by Rastko Koprivica on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct SendhooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SendhooksTests/SendhooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SendhooksTests.swift
+//  SendhooksTests
+//
+//  Created by Rastko Koprivica on 2024-10-15.
+//
+
+import Testing
+@testable import Sendhooks
+
+struct SendhooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
